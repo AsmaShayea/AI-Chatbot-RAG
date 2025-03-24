c462297e720aabefab2a4ee152b6b39d10276c7c
Asma Shayea <asma@Asmas-MacBook-Pro.local>
2025-03-25 02:30:39 +0300

    env

--- app/database.py
+++ app/database.py
@@ -4,8 +4,15 @@ from pymongo.server_api import ServerApi
 from urllib.parse import quote_plus
 
 def get_database():
-    client = MongoClient("mongodb://localhost:27017/", server_api=ServerApi('1'))
-    db = client["chatbot_saas"]
+    username = "admin"
+    password = "M0nG0@dm1n!2025$"
+    encoded_password = quote_plus(password)
+    connection_string = f"mongodb://{username}:{encoded_password}@16.16.57.62:27017/admin?authSource=admin"
+    
+    client = MongoClient(connection_string, server_api=ServerApi('1'))
+    db = client["chatbot"]
+    # client = MongoClient("mongodb://localhost:27017/", server_api=ServerApi('1'))
+    # db = client["chatbot_saas"]
     return db
 
 db = get_database()
